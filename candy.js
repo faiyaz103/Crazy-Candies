27496e66c2210f85a0b64f22d5ee2b4a57b971bd
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-02-03 21:35:28 +0600

    Drag Functionality Added

--- candy.js
+++ candy.js
@@ -6,6 +6,9 @@ var rows=9;
 var columns=9;
 var score=0;
 
+var currCandy;
+var otherCandy;
+
 //When webpage loads
 window.onload = function(){
     startGame();
@@ -31,6 +34,14 @@ function startGame(){
             tile.id = r.toString()+"-"+c.toString(); //Generates id 0-1, 1-2 like these
             tile.src = "./images/"+randomCandy()+".png"; //Generates <img> src
 
+            // Drag Functionality
+            tile.addEventListener("dragstart", dragStart); //Click candy, start process
+            tile.addEventListener("dragover", dragOver); //Clicking candy and moving with mouse
+            tile.addEventListener("dragenter", dragEnter); //Dragging over another candy
+            tile.addEventListener("dragleave", dragLeave); //Leave over another candy
+            tile.addEventListener("drop", dragDrop); //Dropping over another candy
+            tile.addEventListener("dragend", dragEnd); //After drag completed and swapping done
+
             document.getElementById("board").append(tile); //Add tile to board
 
             row.push(tile);
@@ -40,4 +51,52 @@ function startGame(){
     }
 
     console.log(board);
+}
+
+function dragStart(){
+    currCandy=this;
+}
+
+function dragOver(e){
+    e.preventDefault();
+}
+
+function dragEnter(e){
+    e.preventDefault();
+}
+
+function dragLeave(){
+    
+}
+
+function dragDrop(){
+    otherCandy=this;
+}
+
+function dragEnd(){
+
+    let currCoords=currCandy.id.split("-"); //0-1 -> ["0", "1"]
+    let r=parseInt(currCoords[0]);
+    let c=parseInt(currCoords[1]);
+
+    let otherCoords=otherCandy.id.split("-");
+    let ro=parseInt(otherCoords[0]);
+    let co=parseInt(otherCoords[1]);
+
+    let mvLeft = (co==c-1 && r==ro);
+    let mvRight = (co==c+1 && r==ro);
+    let mvUp = (ro==r-1 && c==co);
+    let mvDown = (ro==r+1 && c==co);
+
+    let isPoss = (mvLeft || mvRight || mvUp || mvDown);
+
+    if(isPoss){
+        let currImg=currCandy.src;
+        let otherImg=otherCandy.src;
+
+        currCandy.src=otherImg;
+        otherCandy.src=currImg;
+    }
+
+    
 }
